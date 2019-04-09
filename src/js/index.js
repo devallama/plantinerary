4a96195f4b5aff229287598d36270d88fa752cdd
nickallama <me@nicholasneale.com>
2019-04-09 05:30:49 +0100

    Added bootstrap css & js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'Dist/css/style.css';
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
