49464f59fe43ff4c096d394ae1c7179d376896bc
nickallama <me@nicholasneale.com>
2019-04-11 07:10:10 +0100

    Landing page markup

--- src/js/pages/landing.jsx
+++ src/js/pages/landing.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-class PageLanding extends React.Component {
+class Landing extends React.Component {
     render() {
         return (
-            <div>
-                Landing page
-            </div>
+            <section className="section section--landing">
+                <div className="container col-md-6 col-lg-5 col-xl-4">
+                    <h1 className="display-3">Plantinerary</h1>
+                    <h2>Plan your next adventure.</h2>
+                    <p>
+                        Use Plantinerary to find events, tours, restraunts, and more in your next vaction location and add them
+                        to your daily itinerary planner.
+                    </p>
+                    <p>
+                        Your next adventure in one place.
+                    </p>
+                    <div className="mt-5 mb-4">
+                        <Link to="/login" className="mr-4"><button className="btn btn-primary">Login</button></Link>
+                        <button className="btn btn-secondary">Signup</button>
+                    </div>
+                    <a href="#" className="link link--arrow">Continue without logging in</a>
+                </div>
+            </section>
         );
     }
 }
 
-export default PageLanding;
+export default Landing;
