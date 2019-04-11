5a33789e4dd716ce0f1b62a34a126931ac94b52a
nickallama <me@nicholasneale.com>
2019-04-11 07:08:10 +0100

    Implemented firebase and protected route for dashboard

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,28 +1,50 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { firebaseFetchInstance } from 'Actions/firebase-actions';
+import { userIsLoggedIn } from 'Actions/user-actions';
 
-import Page from './pages/page';
+import ProtectedRoute from './routes/ProtectedRoute';
+
+import Page from './pages/templates/page';
 import PageLanding from './pages/landing';
 import PageLogin from './pages/login';
+import PageDashboard from './pages/dashboard';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.props.firebaseFetchInstance();
+
+        console.log("called");
+        console.log(this.props.userIsLoggedIn());
+
+        console.log(this.props);
+    }
+
     render() {
         return (
             <Router>
                 <div>
-                    <nav className="navbar navbar-light bg-light">
-                        <Link to="/" class="navbar-brand">Plantinerary</Link>
-
-                        <ul>
-                            <li>
-                                {/* Can make this do active stuff using NavLink component? I think - its in the docs */}
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/login">Login</Link>
-                            </li>
-                        </ul>
-                    </nav>
+                    <div className="topbar">
+                        <div className="topbar__logo">
+                            Plantinerary
+                        </div>
+                        <div className="topbar__navigation">
+                            <nav className="site-nav">
+                                <ul className="site-nav__list">
+                                    <li className="site-nav__item">
+                                        <Link to="/" className="site-nav__link">Home</Link>
+                                    </li>
+                                    <li className="site-nav__item">
+                                        <Link to="/login" className="site-nav__link">Login</Link>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
 
                     <Route
                         path="/"
@@ -34,8 +56,18 @@ class App extends React.Component {
                     <Route
                         path="/login"
                         render={props => (
-                            <Page {...props} component={PageLogin} title="Login" />
+                            this.props.user.isLoggedIn
+                                ? (
+                                    <Redirect to="/dashboard" />
+                                ) : (
+                                    <Page {...props} component={PageLogin} title="Login" />
+                                )
                         )}
+                    />
+                    <ProtectedRoute
+                        path="/dashboard"
+                        component={PageDashboard}
+                        title="Trips Dashboard"
                     />
                 </div>
             </Router>
@@ -43,4 +75,14 @@ class App extends React.Component {
     }
 }
 
-export default App;
+App.propTypes = {
+    firebaseFetchInstance: PropTypes.func.isRequired,
+    firebase: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    firebase: state.firebase.instance,
+    user: state.user
+});
+
+export default connect(mapStateToProps, { firebaseFetchInstance, userIsLoggedIn })(App);
