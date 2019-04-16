3625145ae47c42e7a0e3a5ca2d06657650c7ac42
nickallama <me@nicholasneale.com>
2019-04-16 05:29:16 +0100

    Redirects user if not logged in

--- src/js/routes/ProtectedRoute.jsx
+++ src/js/routes/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter as Route, Redirect } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { userIsLoggedIn } from 'Actions/user-actions';
 
@@ -9,24 +9,27 @@ import Page from '../pages/templates/page';
 class ProtectedRoute extends Route {
     constructor(props) {
         super(props);
+
+        this.props.userIsLoggedIn();
     }
 
     render() {
-        const props = this.props;
+        const { props } = this;
 
         if (props.user.isLoggedIn == true) {
+            console.log("this");
             return (
                 <Page {...props} component={props.component} title={props.title} />
             );
         } else if (props.user.isLoggedIn == false) {
+            console.log("that");
             return (
-                <Redirect to="/" />
+                <Redirect to={this.props.redirect || "/"} />
             );
         } else {
+            console.log("those");
             // Could put loading thing here
-            return (
-                <h1>Loading...</h1>
-            )
+            return null;
         }
     }
 }
