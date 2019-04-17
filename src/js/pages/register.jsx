2d335d36e139fb4a3dce6d0762bbdbae7b8f72af
nickallama <me@nicholasneale.com>
2019-04-17 04:59:54 +0100

    Added register form to register page

---
+++ src/js/pages/register.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import RegisterForm from '../components/RegisterForm';
+
+class Register extends React.Component {
+    render() {
+        return (
+            <div>
+                <RegisterForm />
+            </div>
+        );
+    }
+}
+
+export default Register;
