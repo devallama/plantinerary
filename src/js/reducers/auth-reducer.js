7b810f95193e7b16b9d08dbaf9dcd7108955b68a
nickallama <me@nicholasneale.com>
2019-04-18 06:48:48 +0100

    Added handler for auth_login_anon type

--- src/js/reducers/auth-reducer.js
+++ src/js/reducers/auth-reducer.js
@@ -1,4 +1,4 @@
-import { AUTH_LOGIN, AUTH_REGISTER } from 'Actions/types';
+import { AUTH_LOGIN, AUTH_REGISTER, AUTH_LOGIN_ANON } from 'Actions/types';
 
 const initialState = {
     response: {
@@ -11,11 +11,11 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case AUTH_LOGIN:
         case AUTH_REGISTER:
+        case AUTH_LOGIN_ANON:
             return {
                 ...state,
                 response: action.data.response
             };
-
         default:
             return state;
     }
