fc32ee31d4c58674a245c818bbc2c04f0f6501cf
nickallama <me@nicholasneale.com>
2019-04-18 06:47:56 +0100

    Login user anonymously

---
+++ src/js/components/LoginAnon/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { authLoginAnon } from 'Actions/auth-actions';
+
+class LoginAnon extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.props.authLoginAnon();
+    }
+
+    render() {
+        console.log(this.props);
+        return (
+            <p>
+                Logging in anonymously...
+            </p>
+        );
+    }
+}
+
+LoginAnon.propTypes = {
+    authLoginAnon: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => {
+    return ({
+        auth: state.auth.response
+    });
+}
+
+export default connect(mapStateToProps, { authLoginAnon })(LoginAnon);
