f426bdf90382ef69fdcd22bc33d8ec455e20388d
nickallama <me@nicholasneale.com>
2019-04-23 08:35:15 +0100

    added user fetch type to reducer

--- src/js/reducers/user-reducer.js
+++ src/js/reducers/user-reducer.js
@@ -1,4 +1,5 @@
 import { USER_IS_LOGGED_IN } from 'Actions/types';
+import { USER_FETCH } from '../actions/types';
 
 const initialState = {
     isLoggedIn: null,
@@ -11,6 +12,11 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 isLoggedIn: action.data
+            };
+        case USER_FETCH:
+            return {
+                ...state,
+                user: action.data
             };
         default:
             return state;
