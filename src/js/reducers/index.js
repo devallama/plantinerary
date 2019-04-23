c19f999a152194c89131d07cacfabbf1ce141cd9
nickallama <me@nicholasneale.com>
2019-04-23 08:34:55 +0100

    Added trips reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import userReducer from './user-reducer';
 import firebaseReducer from './firebase-reducer';
 import authReducer from './auth-reducer';
+import tripsReducer from './trips-reducer';
 
 export default combineReducers({
     auth: authReducer,
     firebase: firebaseReducer,
-    user: userReducer
+    user: userReducer,
+    trips: tripsReducer
 });
