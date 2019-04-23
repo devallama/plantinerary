36fac653008357de799330f56ea09cceeaf3d7ae
nickallama <me@nicholasneale.com>
2019-04-23 08:34:43 +0100

    use trips dashboard  component

--- src/js/pages/dashboard.jsx
+++ src/js/pages/dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import TripsDashboard from 'Components/TripsDashboard';
+
 class Dashboard extends React.Component {
     render() {
         return (
             <div className="container">
-                Dashboard
+                <TripsDashboard />
             </div>
         );
     }
