e04ac98921d134f7909f3ef81fc1db969f13d603
nickallama <me@nicholasneale.com>
2019-04-23 05:19:01 +0100

    Moved header to header component

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { firebaseFetchInstance } from 'Actions/firebase-actions';
 
@@ -11,6 +11,7 @@ import Page from './pages/templates/page';
 import PageLanding from './pages/landing';
 import PageLogin from './pages/login';
 import PageRegister from './pages/register';
+import PageLogout from './pages/logout';
 import PageDashboard from './pages/dashboard';
 import PageLoginAnon from './pages/login-anon';
 
@@ -24,59 +25,45 @@ class App extends React.Component {
     render() {
         return (
             <Router>
-                <div>
-                    <div className="topbar">
-                        <div className="topbar__logo">
-                            Plantinerary
-                        </div>
-                        <div className="topbar__navigation">
-                            <nav className="site-nav">
-                                <ul className="site-nav__list">
-                                    <li className="site-nav__item">
-                                        <Link to="/" className="site-nav__link">Home</Link>
-                                    </li>
-                                    <li className="site-nav__item">
-                                        <Link to="/login" className="site-nav__link">Login</Link>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-
-                    <Switch>
-                        <Route
-                            path="/"
-                            exact
-                            render={props => (
-                                <Page {...props} component={PageLanding} title="Home" />
-                            )}
-                        />
-                        <NouserRoute
-                            path="/login"
-                            component={PageLogin}
-                            title="Login"
-                            redirect="/dashboard"
-                        />
-                        <NouserRoute
-                            path="/login-anon"
-                            component={PageLoginAnon}
-                            title="Login Anonymously"
-                            redirect="/dashboard"
-                        />
-                        <NouserRoute
-                            path="/register"
-                            component={PageRegister}
-                            title="Register"
-                            redirect="/dashboard"
-                        />
-                        <ProtectedRoute
-                            path="/dashboard"
-                            component={PageDashboard}
-                            title="Trips Dashboard"
-                            redirect="/"
-                        />
-                    </Switch>
-                </div>
+                <Switch>
+                    <Route
+                        path="/"
+                        exact
+                        render={props => (
+                            <Page {...props} component={PageLanding} title="Home" />
+                        )}
+                    />
+                    <NouserRoute
+                        path="/login"
+                        component={PageLogin}
+                        title="Login"
+                        redirect="/dashboard"
+                    />
+                    <NouserRoute
+                        path="/login-anon"
+                        component={PageLoginAnon}
+                        title="Login Anonymously"
+                        redirect="/dashboard"
+                    />
+                    <NouserRoute
+                        path="/register"
+                        component={PageRegister}
+                        title="Register"
+                        redirect="/dashboard"
+                    />
+                    <ProtectedRoute
+                        path="/logout"
+                        component={PageLogout}
+                        title="Logout from Plantinerary"
+                        redirect="/"
+                    />
+                    <ProtectedRoute
+                        path="/dashboard"
+                        component={PageDashboard}
+                        title="Trips Dashboard"
+                        redirect="/"
+                    />
+                </Switch>
             </Router>
         )
     }
