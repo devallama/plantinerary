f802d6a84caf24688b8dd214c2a22d8b1234137d
nickallama <me@nicholasneale.com>
2019-04-30 14:38:26 +0100

    Added validation to end date to make it after startdate

--- src/js/components/CreateTripForm/index.jsx
+++ src/js/components/CreateTripForm/index.jsx
@@ -5,6 +5,7 @@ import { tripsCreate } from 'Actions/trips-actions';
 
 import Form from 'Components/Form';
 import FormInput from 'Components/FormInput';
+import FormTextarea from 'Components/FormTextarea';
 
 class CreateTripForm extends React.Component {
     constructor(props) {
@@ -17,6 +18,11 @@ class CreateTripForm extends React.Component {
                     format: "This name must be a valid name"
                 }
             },
+            description: {
+                errorMessages: {
+                    required: "Please enter a description for the trip"
+                }
+            },
             type: {},
             startDate: {
                 errorMessages: {
@@ -27,7 +33,11 @@ class CreateTripForm extends React.Component {
             endDate: {
                 errorMessages: {
                     required: "Please select an end date for the trip",
-                    format: "This must be a valid date"
+                    format: "This must be a valid date",
+                    greaterThan: "The end date needs to be after the start date"
+                },
+                customValidation: {
+                    greaterThan: (field, fields) => field.value >= fields.startDate.value
                 }
             },
             country: {
@@ -51,7 +61,7 @@ class CreateTripForm extends React.Component {
                 <div className="modal-dialog modal-dialog-scrollable" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalScrollableTitle">Modal title</h5>
+                            <h5 className="modal-title" id="exampleModalScrollableTitle">Create a trip</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
@@ -59,18 +69,15 @@ class CreateTripForm extends React.Component {
                         <div className="modal-body">
                             <Form fields={this.fields} className="form form--createTrip" noValidate multiSubmit={true}
                                 submitMethod={(fields) => { this.hideModal(); this.props.tripsCreate(fields); }}>
-                                <h3>Create a trip</h3>
                                 <FormInput label="Name" name="name" required />
+                                <FormTextarea label="Description" name="description" required />
                                 <FormInput label="Type" name="type" />
                                 <FormInput label="Start date" name="startDate" type="date" required />
                                 <FormInput label="End date" name="endDate" type="date" required />
                                 <FormInput label="Country" name="country" required />
+                                <button className="btn btn-secondary mr-2" type="button" ref={this.closeModal} data-dismiss="modal">Cancel</button>
                                 <button className="btn btn-primary" type="submit">Create</button>
                             </Form>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" ref={this.closeModal} data-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
