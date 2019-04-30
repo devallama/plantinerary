4bf8a0fb201045083077afee4ec30be8a0642aba
nickallama <me@nicholasneale.com>
2019-04-30 16:27:00 +0100

    Added image to result

--- src/js/components/TripPlanner/DayView/Search/Results/index.jsx
+++ src/js/components/TripPlanner/DayView/Search/Results/index.jsx
@@ -9,9 +9,16 @@ class Results extends React.Component {
         const resultCardEls = this.props.results.map(result =>
             <div className="card mb-2" key={result.stop_id}>
                 <div className="card-body">
-                    <h5 className="card-title">{result.name}</h5>
-                    <p className="card-text">{result.summary}</p>
-                    <small className="text-muted">{result.location.address}</small>
+                    <div className="row">
+                        <div className="col-4">
+                            <img src={result.picture.url} className="img-fluid rounded" alt={result.name} />
+                        </div>
+                        <div className="col-8">
+                            <h5 className="card-title">{result.name}</h5>
+                            <p className="card-text">{result.summary}</p>
+                            <small className="text-muted">{result.location.address}</small>
+                        </div>
+                    </div>
                 </div>
                 <div className="card-footer">
                     <button type="button" className="btn btn-primary" onClick={() => this.props.selectEvent(result)}>Add to itinerary</button>
