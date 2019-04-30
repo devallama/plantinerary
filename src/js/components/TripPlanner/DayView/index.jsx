5c91c47f7a1155315f33765914171d6429fa22e1
nickallama <me@nicholasneale.com>
2019-04-30 16:26:51 +0100

    Added itinerary info

--- src/js/components/TripPlanner/DayView/index.jsx
+++ src/js/components/TripPlanner/DayView/index.jsx
@@ -38,17 +38,49 @@ class DayView extends React.Component {
                     <li onClick={event => this.selectTime(event, i)} style={{ cursor: 'pointer' }} key={i} className="list-group-item">
                         {`${i}:00`}
                         {itineraryItem &&
-                            <div className="itinerary-list">
-                                <div className="itinerary-list__item itinerary-list__item--name">
-                                    <h5 className="itinerary-list__header">{itineraryItem.event.name}</h5>
+                            <div>
+                                <div className="itinerary-list">
+                                    <div className="itinerary-list__item itinerary-list__item--name">
+                                        <h5 className="itinerary-list__header">{itineraryItem.event.name}</h5>
+                                    </div>
+                                    <div className="itinerary-list__item">
+                                        <button className="btn btn-primary btn-sm mr-2" data-toggle="collapse" href={`#${itineraryItem.event.stop_id}`}>View details</button>
+                                        <button className="btn btn-secondary btn-sm mr-2" onClick={() => this.props.focusItineraryItem(itineraryItem)}>Focus on map</button>
+                                        <button className="btn btn-danger btn-sm" onClick={() => this.props.itineraryItemDelete(this.props.tripId, itineraryItem.id)}>Delete</button>
+                                    </div>
                                 </div>
-                                <div className="itinerary-list__item">
-                                    <button className="btn btn-primary btn-sm mr-2">View details</button>
-                                    <button className="btn btn-secondary btn-sm mr-2" onClick={() => this.props.focusItineraryItem(itineraryItem)}>Focus on map</button>
-                                    <button className="btn btn-danger btn-sm" onClick={() => this.props.itineraryItemDelete(this.props.tripId, itineraryItem.id)}>Delete</button>
+
+                                <div className="row">
+                                    <div className="col">
+                                        <div className="collapse multi-collapse mt-4" id={itineraryItem.event.stop_id}>
+                                            <div className="card card-body">
+                                                <img src={itineraryItem.event.picture.url} className="img-fluid rounded" alt={itineraryItem.event.name} />
+                                                <table className="table">
+                                                    <tbody>
+                                                        <tr>
+                                                            <th scope="row">Name</th>
+                                                            <td>{itineraryItem.event.name}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th scope="row">Summary</th>
+                                                            <td>{itineraryItem.event.summary}</td>
+                                                        </tr>
+                                                        {itineraryItem.event.price_retail.from_price &&
+                                                            <tr>
+                                                                <th scope="row">Prices from</th>
+                                                                <td>{`${itineraryItem.event.price_retail.from_price} ${itineraryItem.event.price_retail.currency}`}</td>
+                                                            </tr>
+                                                        }
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         }
+
+
 
                     </li>
                 );
