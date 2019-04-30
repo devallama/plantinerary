965491fec2e5492035d6cc94b1c194bed6436141
nickallama <me@nicholasneale.com>
2019-04-30 14:37:59 +0100

    Added new types for actions

--- src/js/actions/types.js
+++ src/js/actions/types.js
@@ -15,6 +15,21 @@ export const AUTH_LOGOUT = 'AUTH_LOGOUT';
 
 /* Trips Action types */
 
+export const TRIP_FETCH = 'TRIP_FETCH';
 export const TRIPS_FETCH = 'TRIPS_FETCH';
-export const TRIPS_CREATE = 'TRIPS_CREATE';
-export const TRIPS_EDIT = 'TRIPS_EDIT';
+export const TRIP_CREATE = 'TRIP_CREATE';
+export const TRIP_EDIT = 'TRIP_EDIT';
+export const TRIP_DELETE = 'TRIP_DELETE';
+
+/* Events Action types */
+
+export const ITINERARY_FETCH = 'ITINERARY_FETCH';
+export const ITINERARY_ITEM_SET = 'ITERARY_ITEM_SET';
+export const ITINERARY_ITEM_CREATE = 'ITINERARY_ITEM_CREATE';
+export const ITINERARY_ITEM_UPDATE = 'ITINERARY_ITEM_UPDATE';
+export const ITINERARY_ITEM_DELETE = 'ITINERARY_ITEM_DELETE';
+
+/* Autoura Action types */
+
+export const AUTOURA_SEARCH_STOPS = 'AUTOURA_SEARCH_STOPS';
+export const AUTOURA_GET_STOP_TYPES = 'AUTOURA_GET_STOP_TYPES';
