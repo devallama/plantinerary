7ca4e93256a34fcbca56febd1f4c3c664d0e4ceb
nickallama <me@nicholasneale.com>
2019-05-01 01:34:49 +0100

    Removed comments

--- src/js/index.js
+++ src/js/index.js
@@ -4,14 +4,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
-// import firebase from 'firebase';
-// import firebaseConfig from './firebase.confid';
-
 import 'bootstrap';
 
 import App from './App';
-
-// firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
     <Provider store={store}>
